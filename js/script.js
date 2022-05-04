e23cc9bdc8dbeb7469ee20dae5e892bd6da01532
stefanocravotta <stefano.cravotta8@gmail.com>
2022-05-04 16:28:11 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 Stampare all’interno di una lista, un item per ogni todo.
 Se la proprietà `done` è uguale a `true`, visualizzare il testo del todo ~~sbarrato~~.
 **MILESTONE 2**
-Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+Visualizzare a fianco ad ogni item una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 **MILESTONE 3**
 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
@@ -31,5 +31,10 @@ const app = new Vue({
         done : false
       },
     ]
+  },
+  methods: {
+    removeTodo(index){
+      this.toDos.splice(index,1)
+    }
   },
 })
